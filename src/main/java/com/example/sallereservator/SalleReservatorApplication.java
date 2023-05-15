e0eaa5460d388bbfe7f217cabfd343314cec54f6
tom-fondville <tomfondville@gmail.com>
2023-05-15 08:19:52 +0200

    init

---
+++ src/main/java/com/example/sallereservator/SalleReservatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.sallereservator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalleReservatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalleReservatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sallereservator/SalleReservatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sallereservator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalleReservatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
